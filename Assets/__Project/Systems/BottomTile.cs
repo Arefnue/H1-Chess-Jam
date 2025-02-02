815af9ce435495d666966129c47e05c63551f15b
Arif Mertcan Kara <arifmertcankara@gmail.com>
2025-02-02 21:44:20 +0300

    WIP: Better visuals

---
+++ Assets/__Project/Systems/BottomTile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace __Project.Systems
+{
+    public class BottomTile : MonoBehaviour
+    {
+        [SerializeField] private Material whiteMat;
+        [SerializeField] private Material blackMat;
+        [SerializeField] private Renderer rend;
+
+
+        private void Awake()
+        {
+            rend.material = (transform.position.x + transform.position.z) % 2 == 0 ? whiteMat : blackMat;
+        }
+    }
+}
