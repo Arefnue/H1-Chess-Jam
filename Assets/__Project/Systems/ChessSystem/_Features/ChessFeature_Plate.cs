b9a6d2bca9e12a5492db886f3e52eec325fd1221
Arif Mertcan Kara <arifmertcankara@gmail.com>
2025-02-11 19:03:59 +0300

    feat: Multi door

--- Assets/__Project/Systems/ChessSystem/_Features/ChessFeature_Plate.cs
+++ Assets/__Project/Systems/ChessSystem/_Features/ChessFeature_Plate.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Collections.Generic;
 using System.Linq;
 using __Project.Systems.ChessSystem._Grid;
 using __Project.Systems.ChessSystem._Pieces;
@@ -19,7 +20,7 @@ namespace __Project.Systems.ChessSystem._Features
         #region Cache
         public ChessColorEnum TargetColor => targetColor;
         [ShowInInspector,ReadOnly]public Vector3Int OccupiedTilePosition { get; private set; }
-        public ChessFeature_Door TargetDoor { get; private set; }
+        public List<ChessFeature_Door> TargetDoorList { get; private set; } = new List<ChessFeature_Door>();
         public bool IsDoorOpened { get; private set; }
         #endregion
        
@@ -27,18 +28,26 @@ namespace __Project.Systems.ChessSystem._Features
         {
             base.Build(gridLayerChess);
             PlaceOnTile(GridLayerChess.GetWorldPos(transform.position));
-            
-            TargetDoor = GridLayerChess.FeatureList
-                .OfType<ChessFeature_Door>()
-                .FirstOrDefault(door => door.TargetColor == this.TargetColor);
 
-            if (TargetDoor)
+            var tempList = GridLayerChess.FeatureList
+                .OfType<ChessFeature_Door>().ToList();
+            TargetDoorList.Clear();
+            foreach (var door in tempList)
             {
-                TargetDoor.SetDoor(this);
+                if (door.TargetColor != TargetColor)
+                    continue;
+                TargetDoorList.Add(door);
             }
+
+            DOVirtual.DelayedCall(0.02f, () =>
+            {
+                foreach (var door in TargetDoorList)
+                {
+                    door.SetDoor(this);
+                }
+            }, false);
             RBuss.OnEvent<ChessREvents.PieceMoveFinishedREvent>().Subscribe(ev =>
             {
-                
                 UpdateFeature();
             }).AddTo(gameObject);
         }
@@ -50,17 +59,21 @@ namespace __Project.Systems.ChessSystem._Features
             if (piece)
             {
                 if (IsDoorOpened) return;
-                "Opened".NLog();
-                TargetDoor.Open();
+                foreach (var door in TargetDoorList)
+                {
+                    door.Open();
+                }
                 IsDoorOpened = true;
                 animator.SetTrigger("Press");
             }
             else
             {
                 if (!IsDoorOpened) return;
-                "Closed".NLog();
                 IsDoorOpened = false;
-                TargetDoor.Close();
+                foreach (var door in TargetDoorList)
+                {
+                    door.Close();
+                }
                 animator.SetTrigger("UnPress");
             }
             
